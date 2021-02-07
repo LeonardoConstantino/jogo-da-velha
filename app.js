e6b875165eb3e71bdef02c493ed78a3f9b6fb5b5
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2021-02-07 11:50:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,23 +19,34 @@ var xis = "imagens/xis.png"
 var bola = "imagens/bola.png"
 
 function animaGanhador(n1, n2, n3) {
-    cels[n1].setAttribute('class', 'animaimg')
-    cels[n2].setAttribute('class', 'animaimg')
-    cels[n3].setAttribute('class', 'animaimg')
+    // var imgs = document.querySelectorAll('#cel0 .img')
+
+    var imgs = [
+        document.querySelectorAll(`#cel${n1} .img`),
+        document.querySelectorAll(`#cel${n2} .img`),
+        document.querySelectorAll(`#cel${n3} .img`)
+    ]
+    imgs[0].setAttribute('class', 'animaimg')
+    imgs[1].setAttribute('class', 'animaimg')
+    imgs[3].setAttribute('class', 'animaimg')
+
+    // cels[n1].setAttribute('class', 'animaimg')
+    // cels[n2].setAttribute('class', 'animaimg')
+    // cels[n3].setAttribute('class', 'animaimg')
 }
 
 
-// function addouRemoverEventoClik(event) {
-//     cels[0].setAttribute('class', event)
-//     cels[1].setAttribute('class', event)
-//     cels[2].setAttribute('class', event)
-//     cels[3].setAttribute('class', event)
-//     cels[4].setAttribute('class', event)
-//     cels[5].setAttribute('class', event)
-//     cels[6].setAttribute('class', event)
-//     cels[7].setAttribute('class', event)
-//     cels[8].setAttribute('class', event)
-// }
+function addouRemoverEventoClik(event) {
+    cels[0].setAttribute('class', event)
+    cels[1].setAttribute('class', event)
+    cels[2].setAttribute('class', event)
+    cels[3].setAttribute('class', event)
+    cels[4].setAttribute('class', event)
+    cels[5].setAttribute('class', event)
+    cels[6].setAttribute('class', event)
+    cels[7].setAttribute('class', event)
+    cels[8].setAttribute('class', event)
+}
 
 
 function adicionaXO(celula, xo) {
@@ -64,54 +75,72 @@ function atualizaTabuleiroX(pos, xo) {
 }
 
 function x0(cel = '0') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x1(cel = '1') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x2(cel = '2') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x3(cel = '3') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x4(cel = '4') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x5(cel = '5') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x6(cel = '6') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x7(cel = '7') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
 }
 
 function x8(cel = '8') {
+    addouRemoverEventoClik('naoclik')
+    cels[cel].setAttribute("onclick", '')
     atualizaTabuleiroX(cel, 'x')
     adicionaXO(cels[cel], xis)
     ganhador()
@@ -137,6 +166,7 @@ function jogadaMaquina(ran = Math.floor(Math.random() * 9)) {
             maxJogMaq++
         }
     }
+    addouRemoverEventoClik('clik')
 }
 
 
@@ -145,8 +175,8 @@ function jogadaMaquina(ran = Math.floor(Math.random() * 9)) {
 function conferirGanhador() {
     if (tabuleiro[0] == tabuleiro[1] && tabuleiro[0] ==
         tabuleiro[2] && tabuleiro[1] == tabuleiro[2]) {
-        if (tabuleiro[0] && tabuleiro[1] && tabuleiro[2] == 'o') {
             animaGanhador(0, 1, 2,)
+        if (tabuleiro[0] && tabuleiro[1] && tabuleiro[2] == 'o') {
             resultadoJogo = 1
             console.log('o GANHOU LINHA 1')
         } else {
